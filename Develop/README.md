f554683970a89ad395f97d3941a2ceee5d9ac3d8
Katelyn Rakonjac King <katelynking@Katelyns-MacBook-Air.local>
2022-06-12 21:28:28 -0400

    questions and info edited. need to work on license selection and a readme for project

--- Develop/README.md
+++ Develop/README.md
@@ -1,30 +1,59 @@
-# README.md Generator
+# Generator
 ## Description
--Motivation: The motivation behind this project was to help programmers create README files more efficiently
+The motivation of this project was motivation.
 
--Purpose: This project was built in order to give programmers more time to work on their project instead of a README file
+This project solves the issue of purpose.
 
--Solution: It allows programmers to generate a README file without having to start from scratch
+Guten tag
 
-## Installation
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Questions](#questions)
 
-This project requires the following: VSCode, Node.js
+<a name="installation"></a>
+## Installation 
+
+This project requires the following: VSCode
+
+The following code will be necessary to utilize this project:
+
+``` npm init -y ```
+<a name="usage"></a>
+## Usage 
+Link to deployed application: [Generator](link)
+
+First, this. Then, this. Next, this. Finally, this.
 
 
-``` npm init -y and npm i ```
-
-## Credits
-
-Contributor: [Katelyn King](https://github.com/katelynking)
-
+<a name="license"></a>
 ## License 
 
 MIT License
     
   Copyright (c) 2022 Katelyn King
   
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "README.md Generator"), to deal in the software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software, and to permit persons to whom the software is furnished to do so, subject to the following conditions:
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Generator"), to deal in the software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software, and to permit persons to whom the software is furnished to do so, subject to the following conditions:
   
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
   
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  
+  <a name="contributing"></a>
+  ## Contributing
+  
+  Created by [Katelyn King](https://github.com/katelynking)
+  
+  If you would like to contribute to this project, you can do so by adding an input for a screenshot.
+
+  <a name="test"></a>
+  ## Tests
+  xyz
+ 
+
+  <a name="questions"></a>
+  ## Questions
+  For any questions, please do not hesitate to contact me at katelynking@gmail.com!
--- Develop/pseudo_index.js
+++ Develop/pseudo_index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateRM = ({ creator, title, motivation, purpose, tools, solution, code, github, link}) =>
+const generateRM = ({ creator, title, motivation, purpose, tools, utilization, contribution, code, test, github, link, email, license}) =>
     `# ${title}
 ## Description
 The motivation of this project was ${motivation}.
@@ -11,22 +11,32 @@ This project solves the issue of ${purpose}.
 
 
 ## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Questions](#questions)
 
-
-## Installation
+<a name="installation"></a>
+## Installation 
 
 This project requires the following: ${tools}
 
 The following code will be necessary to utilize this project:
 
 \`\`\` ${code} \`\`\`
-
+<a name="usage"></a>
 ## Usage 
-
 Link to deployed application: [${title}](${link})
 
+${utilization}
+
+
+<a name="license"></a>
 ## License 
 
+${license}
 MIT License
     
   Copyright (c) 2022 ${creator}
@@ -37,14 +47,21 @@ MIT License
   
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
   
+  <a name="contributing"></a>
   ## Contributing
   
-  [${creator}](https://github.com/${github})
+  Created by [${creator}](https://github.com/${github})
+  
+  If you would like to contribute to this project, you can do so by ${contribution}.
 
+  <a name="test"></a>
   ## Tests
-  
-  
+  ${test}
+ 
+
+  <a name="questions"></a>
   ## Questions
+  For any questions, please do not hesitate to contact me via [email](${email}) or [GitHub](https://github.com/${github}) !
 `;
 
 
@@ -52,7 +69,7 @@ const questions = [
     {
         type: 'input',
         name: 'creator',
-        message: 'Who is the creator of this project?',
+        message: 'Please enter the name of the creator of this project:',
     },
     {
         type: 'input',
@@ -71,11 +88,6 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'solution',
-        message: 'What problem does it solve?',
-    },
-    {
-        type: 'input',
         name: 'tools',
         message: 'What is needed for this project to be properly executed?',
     },
@@ -87,12 +99,38 @@ const questions = [
     {
         type: 'input',
         name: 'link',
-        message: 'Please enter the link to the project: ',
+        message: 'Please enter the link to the project:',
+    },
+    {
+        type: 'input',
+        name: 'utilization',
+        message: 'Please provide step by step details on how to utilize this project.'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'If one would like to contribute to this project, they can do so by:',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do you test this project?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email:',
     },
     {
         type: 'input',
         name: 'github',
         message: 'Please enter your GitHub name: ',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select which license you would like to include in your README.md.',
+        choices: ['MIT license', 'Other']
     }
 ];
 
@@ -102,15 +140,10 @@ function init() {
         .prompt(questions)
         .then((answers) => {
             const readmeContent = generateRM(answers);
-            fs.writeFile('README.md', readmeContent, (err) => err 
-            ? console.log(err) 
-            : console.log('Successfully created new README.md!'));
+            fs.writeFile('README.md', readmeContent, (err) => err
+                ? console.log(err)
+                : console.log('Successfully created new README.md!'));
         });
 };
 
 init();
-
-
-// ## Usage
-
-// ![${altText}](${imagePath})
