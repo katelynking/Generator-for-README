88349265fd1b2ff814e303fb6f98f6736a85dc2b
Katelyn Rakonjac King <katelynking@Katelyns-MacBook-Air.local>
2022-06-15 15:20:04 -0400

    ready for deployment -- license info still does not show up on README.md file

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# Generator for README
+# Generator for README.md file
 
+## Description
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+The motivation of this project was to generate a README.md file quickly and efficiently.
+
+This project solves the issue of spending more time than necessary creating a README.md file from scratch.
+
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Questions](#questions)
+
+<a name="installation"></a>
+## Installation 
+
+This project requires the following: VSCode, Node.js
+
+The following code will be necessary to utilize this project:
+
+``` npm init -y and npm i ```
+<a name="usage"></a>
+## Usage 
+Link to deployed application: [Generator for README.md file](https://github.com)
+
+1. Download VSCode and Node.js if not already downloaded. 2. Create a repo in GitHub and clone it into a new project folder 3. Create your project. 4. Open 'Generator for README.md file' in VSCode. 5. Open an integrated terminal to download npm init -y and npm i. 6. Run the code using 'node index.js' 7. Fill in the information in the prompts. 8. Copy and paste the generated README.md file into your project folder.  
+
+
+<a name="license"></a>
+## License 
+MIT
+
+  
+  <a name="contributing"></a>
+  ## Contributing
+  
+  Created by [Katelyn King](https://github.com/katelynking)
+  
+  If you would like to contribute to this project, you can do so by creating a way for the license selection to upload the entire license information in the README.md file, and by creating a way for a screenshot to be uploaded into the README.md file..
+
+  <a name="test"></a>
+  ## Tests
+  This project was tested with VSCode and by creating this README.md file that you are reading right now. 
+ 
+
+  <a name="questions"></a>
+  ## Questions
+  GitHub username: [katelynking](https://github.com/katelynking)
+  Email: katelynking112@gmail.com
+  
+  Please feel free to contact me with any questions.
---
+++ index.js
@@ -0,0 +1,97 @@
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+const questions = [
+   
+    {
+        type: 'input',
+        name: 'creator',
+        message: 'Please enter the name of the creator of this project:',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project?',
+    },
+    {
+        type: 'input',
+        name: 'motivation',
+        message: 'What was the motivation behind this project?',
+    },
+    {
+        type: 'input',
+        name: 'purpose',
+        message: 'Why did you build this project?',
+    },
+    {
+        type: 'input',
+        name: 'tools',
+        message: 'What is needed for this project to be properly executed?',
+    },
+    {
+        type: 'input',
+        name: 'code',
+        message: 'What coding needs to be installed?',
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Please enter the link to the project:',
+    },
+    {
+        type: 'input',
+        name: 'utilization',
+        message: 'Please provide step by step details on how to utilize this project.'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'If one would like to contribute to this project, they can do so by:',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do you test this project?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email:',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub name: ',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select which license you would like to include in your README.md.',
+        choices: ['BSD', 'MIT', 'GNU']
+    }
+    
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+            return console.log(err);
+        } else {
+            return 'Successfully created new README.md!';
+        }
+    });
+}
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            writeToFile('README.md', data, (err) => err
+                ? console.log(err)
+                : console.log('Successfully created new README.md!'));
+        });
+};
+
+init();
